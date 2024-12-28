9fb88dff7940ac5d78d02e6024fdc8d4d3786aac
JenyaCherny <jenya.cherny@gmail.com>
2024-12-28 18:39:22 -0500

    did experiments in index.js and commented them out

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+let gameList = ["White Elefant", "Mr and Mrs Smith", "Fanty", "Funny News"];
+console.log(gameList);
+console.log(gameList[0]);
+console.log(gameList[2]);
+console.log(typeof gameList);
+
+gameList.shift();
+console.log(gameList);
+
+gameList.push("Riddles");
+console.log(gameList);
+
+let numberLucky = [20, 21, 22, 23, 24, 25];
+console.log(numberLucky);
+console.log(typeof numberLucky);
+numberLucky.push(26);
+console.log(numberLucky);
