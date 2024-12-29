75c0d42ef164fd8a7ca3cbd89f2bf7a7b8ec7c5b
JenyaCherny <jenya.cherny@gmail.com>
2024-12-28 20:37:01 -0500

    read assignment.js and questions

--- README.md
+++ README.md
@@ -36,9 +36,8 @@ To view the project locally, you can either open the project files in your code 
 - [x] When finished with experimenting, comment out everything in `index.js`. You can use these as references for future project/assignments.
 
 #### Part 2
-
-- [ ] Navigate to the Assignment Page
-- [ ] Read the assignment overview and output section.
+- [x]Navigate to the Assignment Page
+- [x] Read the assignment overview and output section.
 - [ ] In `assignment.js`:
     - [ ] Create a mutable variable named `shiftValue` and do not assign it a value
     - [ ] Create a mutable variable named `popValue` and do not assign it a value.
--- js/assignment.js
+++ js/assignment.js
@@ -9,7 +9,8 @@ const todos = [
 ];
 
 // Declare your two immutable variables
-
+let shiftValue;
+let popValue;
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -21,12 +22,23 @@ const answerFiveEl = document.getElementById("answer-five");
 const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
+/*console.log(todo.join(','));   #1
+ console.log(todo[2]);           #2
+ let popValue = todo.pop(); console.log(popValue); #3
+ console.log(todo);              #4
+ todo.reverse();                 #5
+ console.log(todo);              #6
+ let shiftValue = todo.shift(); console.log(shiftValue); #7
+ todo.push(shiftValue); todo.push(popValue);             #8
+ console.log(todo);              #9
+*/
+
 function updateAnswerOne() {
     
 }
 
 function updateAnswerTwo() {
-
+   
 }
 
 function removeLastValue() {
@@ -38,7 +50,7 @@ function removeFirstValue() {
 }
 
 function addShiftAndPopValues() {
-
+    
 }
 
 function updateAnswerFour() {
